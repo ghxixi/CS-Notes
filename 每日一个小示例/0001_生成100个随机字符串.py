3458dd76bda8ad84b00131ffa9d7670c896f6e44
ghxixi <49992743+ghxixi@users.noreply.github.com>
2020-04-13 22:34:54 +0800

    Create 0001_生成100个随机字符串.py

---
+++ 每日一个小示例/0001_生成100个随机字符串.py
@@ -0,0 +1,28 @@
+
+
+#------------------------------------
+
+#生成100个key，每个key长度为15位
+
+#------------------------------------
+
+import string
+import random
+
+def create_string(len):
+    str=""
+    for i in range(len):
+        #所有大写字母和数字随机
+        str+=random.choice(string.ascii_uppercase + string.digits)
+    return str
+    
+
+def create_key(count,len):
+    
+    for j in range(count):
+        return create_string(len)
+
+
+if __name__=="__main__":
+    #创建100个key,每个key长度为15位
+    print(create_key(100,15))
