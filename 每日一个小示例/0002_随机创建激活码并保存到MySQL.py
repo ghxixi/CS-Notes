0271e89c860c29a28b6d3ac1bc1888f3c22afe61
ghxixi <49992743+ghxixi@users.noreply.github.com>
2020-04-15 22:01:04 +0800

    随机创建激活码并保存到MySQL.py

--- 每日一个小示例/0002_随机创建激活码并保存到MySQL.py
+++ 每日一个小示例/0002_随机创建激活码并保存到MySQL.py
@@ -1,5 +1,40 @@
 
 
+# ------------------------------------
+
+# 生成100个key，每个key长度为15位
+# 并插入数据库
+# ------------------------------------
+
+import string
+import random
+import psycopg2
+
+def create_string(len):
+    str =""
+    for i in range(len):
+        # 所有大写字母和数字随机
+        str+=random.choice(string.ascii_uppercase + string.digits)
+    return str
+
+
+
+
+if __name__=="__main__":
+    #链接数据库
+    conn = psycopg2.connect(database="omstar_analysis_1585125952729", user="OMStar", password="Aa123456",
+                            host="127.0.0.1", port="15432")
+    # 创建指针对象
+    cur = conn.cursor()
+    # 创建100个key,每个key长度为15位
+    for j in range(100):
+        cur.execute("insert into key_table(key) values ('%s');" % str(create_string(15)))
+        conn.commit()
+    
+    #关闭指针对象
+    cur.close()
+    #关闭链接
+    conn.close()
 
 
 
